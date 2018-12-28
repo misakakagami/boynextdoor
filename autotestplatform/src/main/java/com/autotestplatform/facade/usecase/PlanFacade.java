481da70dd9fde38fb225f79a05f5553e949cb610
zhangwenbo_zkr <zhang_wenbo@sinosoft.com.cn>
2018-12-28 09:10:12 +0800

    update planfacade.java testplanlist.ftl

--- autotestplatform/src/main/java/com/autotestplatform/facade/usecase/PlanFacade.java
+++ autotestplatform/src/main/java/com/autotestplatform/facade/usecase/PlanFacade.java
@@ -40,7 +40,9 @@ public class PlanFacade extends BaseFacade {
         Plan plan = planDao.selectByPrimaryKey(runPlanInDto.getPlanId());
         User loginUser = (User) getSession().getAttribute("loginUser");
         //update data
-        plan.setPlanStatus(runPlanInDto.getStatus());
+        if(plan.getPlanType()==1) {
+        	plan.setPlanStatus(runPlanInDto.getStatus());
+        }
         plan.setUpdateTime(new Date());
         plan.setUpdateUserId(loginUser.getUserId());
         planDao.updateByPrimaryKeySelective(plan);
